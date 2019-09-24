cac844949a8c204ba9d56657da4ec0df249abab9
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2019-09-24 07:34:02 +0000

    update with elastic search

--- django/qanda/service/elasticsearch.py
+++ django/qanda/service/elasticsearch.py
@@ -4,15 +4,13 @@ from django.conf import settings
 from elasticsearch import Elasticsearch, TransportError
 from elasticsearch.helpers import streaming_bulk
 
-FAILED_TO_LOAD_ERROR = 'Failed to load {}: {!r}'
+FAILED_TO_LOAD_ERROR = "Failed to load {}: {!r}"
 
 logging = logging.getLogger(__name__)
 
+
 def get_client():
-    return Elasticsearch(hosts=[
-        {'host':settings.ES_HOST,
-        'port':settings.ES_PORT,}
-    ])
+    return Elasticsearch(hosts=[{"host": settings.ES_HOST, "port": settings.ES_PORT}])
 
 
 def bulk_load(questions):
@@ -20,24 +18,33 @@ def bulk_load(questions):
     es_questions = (q.as_elasticsearch_dict() for q in questions)
 
     for ok, result in streaming_bulk(
-        get_client(),
-        es_questions,
-        index=settings.ES_INDEX,
-        raise_on_error=False,
+        get_client(), es_questions, index=settings.ES_INDEX, raise_on_error=False
     ):
         if not ok:
             all_ok = False
             action, result = result.popitem()
-            logger.error(FAILED_TO_LOAD_ERROR.format(result['_id'], result))
+            logger.error(FAILED_TO_LOAD_ERROR.format(result["_id"], result))
     return all_ok
+
 
 def search_for_questions(query):
     client = get_client()
-    result = client.search(index=settings.ES_INDEX, body={
-        'query':{
-            'match':{
-                'text':query,
-            },
-        },
-    })
-    return (h['_source'] for h in result['hits']['hits'])
+    result = client.search(
+        index=settings.ES_INDEX, body={"query": {"match": {"text": query}}}
+    )
+    return (h["_source"] for h in result["hits"]["hits"])
+
+
+def upsert(question_model):
+    client = get_client()
+    question_dict = question_model.as_elasticsearch_dict()
+    doc_type = question_dict["_type"]
+    del question_dict["_id"]
+    del question_dict["_type"]
+    response = client.update(
+        settings.ES_INDEX,
+        doc_type,
+        id=question_model.id,
+        body={"doc": question_dict, "doc_as_upsert": True},
+    )
+    return response
--- django/user/views.py
+++ django/user/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+
 
 class RegisterView(CreateView):
     template_name = "registration/register.html"
     form_class = UserCreationForm
-
+    success_url = reverse_lazy("user:login")
